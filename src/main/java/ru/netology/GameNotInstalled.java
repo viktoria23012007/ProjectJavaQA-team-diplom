30557c9ae5a138c4131b9ce716d30a5053170bed
Виктория Вареник <viktoria.varenik@yandex.ru>
2022-07-26 22:01:27 +0300

    Обновлен проект и добавлены тесты

--- src/main/java/ru/netology/GameNotInstalled.java
+++
@@ -1,7 +0,0 @@
-package ru.netology;
-
-public class GameNotInstalled extends RuntimeException {
-    public GameNotInstalled(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -41,11 +41,6 @@ public class Player {
      * если игра не была установлена, то надо выкидывать RuntimeException
      */
     public int play(Game game, int hours) {
-        if (!playedTime.containsKey(game)) {
-            throw new GameNotInstalled(
-                    "Данная игра не установлена"
-            );
-        }
         game.getStore().addPlayTime(name, hours);
         int timeGenre = playedTime.getOrDefault(game, hours);
         if (hours < 0) {
